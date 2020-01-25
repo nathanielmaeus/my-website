c6a0b24133ffd6cfba44b228a3bfe35cd9ddf2af
Viacheslav Ermakov <slavaskainet@gmail.com>
2020-01-25 19:04:31 +0300

    fix

--- site-config.js
+++ site-config.js
@@ -1,15 +1,15 @@
 const path = require('path');
 
 module.exports = {
-  siteTitle: `<Viacheslav Ermakov />`,
-  siteTitleShort: `ViErmakov`,
-  siteDescription: `To do site`,
-  siteUrl: `https://todo.com`,
-  themeColor: `#fff`,
-  backgroundColor: `#000`,
+  siteTitle: '<Viacheslav Ermakov />',
+  siteTitleShort: 'ViaErmakov',
+  siteDescription: 'Frontend developer',
+  siteUrl: 'https://viaermakov.netlify.com',
+  themeColor: '#fff',
+  backgroundColor: '#000',
   pathPrefix: null,
   social: {
-    twitter: `gatsbyjs`,
-    fbAppId: `966242223397117`,
+    twitter: 'gatsbyjs',
+    fbAppId: '966242223397117',
   },
 };
