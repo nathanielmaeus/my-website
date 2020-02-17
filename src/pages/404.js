3daa74db3d7c8782dca1c6c01d4635e4da998d5f
Viacheslav Ermakov <slavaskainet@gmail.com>
2020-02-17 22:42:58 +0300

    fix

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
+import MEDIA from 'helpers/mediaTemplates';
 import Layout from 'components/layout';
 
-const NotFound = () => <Layout>Not found.</Layout>;
+const Main = styled.div`
+  margin: 0 4rem;
+
+  ${MEDIA.TABLET`
+    margin: 0 1rem 1rem;  
+  `};
+`;
+
+const NotFound = () => (
+  <Layout>
+    <Main>I still thinking what can I wrire here</Main>
+  </Layout>
+);
 
 export default NotFound;
